ae49f2d7885fdb55c72fb9d83776dd1a75f0bfb7
kirandhande <crosslimits@gmail.com>
2019-06-17 16:03:07 +0530

    fulllogic.php
    this is the full logic for the usage. 
    For separating the list of values by different slots in a sequential order. 
    I will change the code into two files to show as example.

---
+++ fulllogic.php
@@ -0,0 +1,33 @@
+<?php
+	$jsonStr ='{"fp_ids":["538787462f92e2af1a107972","5387822f2f92e2af1a00edee","538799e12f92e2af1a495ad6","538763272f92e2af1aa2bdcd","53879d0a2f92e2af1a53084a","538763172f92e2af1aa28a7f","569e9b4f2f92e2bc28109202","53875a4c2f92e2af1a87474f","57631c552f92e239525994fa","5762e4b52f92e23952d16370","5762cb6f2f92e239529462ba","57630dc32f92e239523628f5","569e742f2f92e2bc28bb89d1","5762fccd2f92e239520c6378","559686557e4a5f64f8d1c042","538745042f92e2af1a44d64e","53878f9b2f92e2af1a29e08e","538768aa2f92e2af1ab372ca","5387a0a72f92e2af1a5e15e8","538777372f92e2af1adfc063","5387706b2f92e2af1acb00ba","538786342f92e2af1a0d38bf","569eb4932f92e2bc2849e1f0","5387a2bd2f92e2af1a6477c3","538780122f92e2af1afa6c8a"],"users":[{"user_id":22050,"name":"Kiran D","asign_leads":8},{"user_id":23330,"name":"Akhil","asign_leads":7},{"user_id":18634,"name":"Kiran Prasad","asign_leads":10}],"assignment_option":"auto","total_assigned_leads":125,"total_selected_leads":200,"total_unassigned_leads":200,"fp_query":"aHR0cHM6Ly9zYWxlcy5idXp6Ym9hcmQuY29tL3Y2L2ZpbmQtcHJvc3BlY3RzP3E9ZXlKZmNYVmxjbmtpT250OUxDSmZjR0Z5WVcxeklqcDdJbUp6SWpwMGNuVmxMQ0pzYjJOaGRHbHZibDl6YjNWeVkyVWlPaUptY0NJc0lteHZZMkYwYVc5dWN5STZXM3NpWTI5MWJuUnllVjlqYjJSbElqb2lkWE1pTENKeVpXZHBiMjRpT2lKT1dTSXNJbXh2WTJGc2FYUjVJam9pVG1WM0lGbHZjbXNpTENKd2JHRmpaVjlwWkNJNklqVmlaRGd3WWpJeVlUVTJZbUlpZlYwc0ltRmtaSEpsYzNNaU9uc2lZMjkxYm5SeWVWOWpiMlJsSWpvaWRYTWlMQ0p5WldkcGIyNGlPaUpPV1NJc0lteHZZMkZzYVhSNUlqb2lUbVYzSUZsdmNtc2lMQ0p3YkdGalpWOXBaQ0k2SWpWaVpEZ3dZakl5WVRVMlltSWlmU3dpYkdsemRHbHVaMTl6YjNWeVkyVWlPaUprY0Y5d2RXSnNhV01pTENKd1lYSjBibVZ5WDJsa0lqb3lNVE0yTENKc1lXNW5YMjVoYldVaU9pSnNZVzVuZFdGblpWOWxiaUlzSW5WelpYSmZhV1FpT2pJeU1EVXdMQ0p5WldkcGIyNWZZbmtpT2lKT1pYY2dXVzl5YXl3Z1Rsa3NJRlZUUVNJc0ltUmxiVzhpT2lKMGNuVmxJaXdpYkdsdGFYUWlPakkxTENKc1lXNW5Jam9pYkdGdVozVmhaMlZmWlc0aUxDSndZV2RsSWpveExDSmpiM1Z1ZEhKNVgyTnZaR1VpT2lKMWN5SXNJbkJzWVdObFgyUmxkR0ZwYkhNaU9uc2ljR3hoWTJWZmFXUWlPaUkxWW1RNE1HSXlNbUUxTm1KaUluMHNJbXRsZVhkdmNtUWlPaUpDWVd0bGNua2lMQ0ozWldKemFYUmxJam94TENKd2JHRmpaVjlwWkhNaU9sc2lOV0prT0RCaU1qSmhOVFppWWlKZExDSnRZWEpyWlhRaU9uc2labkJmYkc5allYUnBiMjRpT2lKT1pYY2dXVzl5YXl3Z1Rsa3NJRlZUUVNJc0ltRmtaSEpsYzNNaU9uc2lZMjkxYm5SeWVWOWpiMlJsSWpvaWRYTWlMQ0p5WldkcGIyNGlPaUpPV1NJc0lteHZZMkZzYVhSNUlqb2lUbVYzSUZsdmNtc2lMQ0p3YkdGalpWOXBaQ0k2SWpWaVpEZ3dZakl5WVRVMlltSWlmU3dpY0d4aFkyVmZaR1YwWVdsc2N5STZleUp3YkdGalpWOXBaQ0k2SWpWaVpEZ3dZakl5WVRVMlltSWlmU3dpY0d4aFkyVmZhV1FpT2lJMVltUTRNR0l5TW1FMU5tSmlJaXdpY0d4aFkyVmZhV1J6SWpwYklqVmlaRGd3WWpJeVlUVTJZbUlpWFN3aVoyVnZiV1YwY25raU9uc2ljR3hoWTJWZmFXUWlPaUkxWW1RNE1HSXlNbUUxTm1KaUluMTlMQ0p6YVdkdVlXeHpJanBiSW1KMWMybHVaWE56WDI1aGJXVWlMQ0ozWldKemFYUmxJaXdpYzNSeVpXVjBYMkZrWkhKbGMzTWlMQ0pzYjJOaGJHbDBlU0lzSW5KbFoybHZiaUlzSW5CdmMzUmhiRjlqYjJSbElpd2lZblZ6YVc1bGMzTmZaVzFoYVd4eklpd2lZblZ6YVc1bGMzTmZjR2h2Ym1WZmJuVnRZbVZ5Y3lJc0ltOXdjRzl5ZEhWdWFYUjVYMmR5WVdSbElpd2lZblZ6YVc1bGMzTmZlV1ZoY2w5bGMzUmhZbXhwYzJobFpDSXNJbkpsZG1WdWRXVmZjMmwwWlNJc0ltVnRjR3h2ZVdWbGMxOXphWHBsSWl3aWVXVmhjbk5mYVc1ZlluVnphVzVsYzNNaUxDSmpiMjUwWVdOMGMxOWpiM1Z1ZENJc0lteHZZMkYwYVc5dWMxOWpiM1Z1ZENKZExDSnpiM0owSWpwN0ltSjVJam9pY21Wc1pYWmhibU5sSWl3aWIzSmtaWElpT2lJaWZTd2lZMkYwWldkdmNubGZkSGx3WlNJNkluQnlaV1JwWTNSbFpDSjlmUSZnc3E9ZXlKdVlXMWxJam9pUW1GclpYSjVJaXdpWm05eWJXRjBkR1ZrWDNaaGJIVmxJam9pUEhOd1lXNCUyQlFtRnJaWEo1UEM5emNHRnVQaUlzSW5SNWNHVWlPaUpyWlhsM2IzSmtJaXdpZEhsd1pWOTBhWFJzWlNJNklrdGxlWGR2Y21RaUxDSnBjMTl0ZFd4MGFYQnNaVjl6YjNWeVkyVWlPbVpoYkhObExDSnpiM1Z5WTJVaU9sdDdJblI1Y0dVaU9pSm1jQ0lzSW5ScGRHeGxJam9pUm1sdVpDQlFjbTl6Y0dWamRITWdLRUoxZW5wQ2IyRnlaQ2tpZlYwc0ltRmpkR2x2Ymw5MGVYQmxJam9pYzJWaGNtTm9YMk5zYVdOckluMCZyYW5kPUxjaFYxaiZyZXF1ZXN0X3NvdXJjZT1nbG9iYWxfc2VhcmNoJnBkPWV5Sm1jRjlzYjJOaGRHbHZiaUk2SWs1bGR5QlpiM0pyTENCT1dTd2dWVk5CSWl3aVlXUmtjbVZ6Y3lJNmV5SmpiM1Z1ZEhKNVgyTnZaR1VpT2lKMWN5SXNJbkpsWjJsdmJpSTZJazVaSWl3aWJHOWpZV3hwZEhraU9pSk9aWGNnV1c5eWF5SXNJbkJzWVdObFgybGtJam9pTldKa09EQmlNakpoTlRaaVlpSjlMQ0p3YkdGalpWOWtaWFJoYVd4eklqcDdJbkJzWVdObFgybGtJam9pTldKa09EQmlNakpoTlRaaVlpSjlMQ0p3YkdGalpWOXBaQ0k2SWpWaVpEZ3dZakl5WVRVMlltSWlMQ0p3YkdGalpWOXBaSE1pT2xzaU5XSmtPREJpTWpKaE5UWmlZaUpkTENKblpXOXRaWFJ5ZVNJNmV5SndiR0ZqWlY5cFpDSTZJalZpWkRnd1lqSXlZVFUyWW1JaWZYMCZyYW5kPTFOVndkdSZ1dG1fY2FtcGFpZ249T1dGXzA0MTAyMDE5","partner_id":2136,"req_type":"Selected/All","campaign_name":"OWF_06132019"}' ; 
+	$data = json_decode($jsonStr, true); 
+	function isvalid($users, $index, $userCoount){
+		echo "<br> ->".$index  .'->' . count($users[$index]['fp_ids']) .'->'. $users[$index]['asign_leads'] ;
+		if( isset($users[$index]['fp_ids']) &&  count($users[$index]['fp_ids']) == $users[$index]['asign_leads']	    ){
+			echo '<br>'; 
+			$index++ ;
+			if($index > ($userCoount-1) ){
+				$index = 0 ; 
+			}
+			isvalid($users, $index, $userCoount) ; 
+		}else{
+			echo "<br>before:".$index; ; 
+			return $index; 
+		}
+	}
+	$users = $data['users'] ; 
+	$userCoount = count($users) ; 
+	$i = 0 ; 
+	foreach($data['fp_ids'] as $item ){
+		echo '<hr>' ; 
+		$i = isvalid($users, $i, $userCoount); 
+		echo "<br>return:"	. $i . '<br>'; 
+		$users[$i]['fp_ids'][] = $item ; 
+		$i++ ;
+		if($i > ($userCoount-1) ){
+			$i = 0 ; 
+		}
+	}
+	echo '<pre>' ; print_r($users); echo '</pre>' ;
+	echo '<pre>' ; print_r($data['fp_ids'] ); echo '</pre>' ;
+?>
